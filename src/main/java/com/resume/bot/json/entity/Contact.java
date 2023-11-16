c215c5449c9468606d75df29b09cf57aecdd6957
te4withm1lk <Bobgjaa@gmail.com>
2023-11-16 15:16:23 +0300

    feat: remove sample code from main class, add getActualValue method for contacts

--- src/main/java/com/resume/bot/json/entity/Contact.java
+++ src/main/java/com/resume/bot/json/entity/Contact.java
@@ -1,6 +1,5 @@
 package com.resume.bot.json.entity;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.AllArgsConstructor;
@@ -8,10 +7,24 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
+import java.util.Arrays;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Contact {
+    private enum TypeContacts {
+        HOME,
+        WORK,
+        CELL,
+        EMAIL;
+
+        @Override
+        public String toString() {
+            return name().toLowerCase();
+        }
+    }
+
     private String comment;
 
     @JsonProperty("need_verification")
@@ -26,4 +39,16 @@ public class Contact {
     private ValuePhoneOrEmail value;
 
     private Boolean verified;
+
+    public String getActualValue() {
+        if (value != null && Arrays.stream(TypeContacts.values()).anyMatch(ct -> ct.toString().equals(type.getName()))) {
+            String email = value.getEmail();
+            Phone phone = value.getPhone();
+            return email != null && TypeContacts.HOME.toString().equals(email)
+                    ? email
+                    : phone != null ? phone.getFormatted()
+                    : null;
+        }
+        return null;
+    }
 }
