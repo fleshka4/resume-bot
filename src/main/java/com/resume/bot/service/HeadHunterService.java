1319e186ba2416a50aa21c65538ef007baf48d67
te4withm1lk <Bobgjaa@gmail.com>
2023-12-07 23:56:22 +0300

    feat: add kostyl to parse json to java

--- src/main/java/com/resume/bot/service/HeadHunterService.java
+++ src/main/java/com/resume/bot/service/HeadHunterService.java
@@ -1,5 +1,6 @@
 package com.resume.bot.service;
 
+import com.resume.bot.json.JsonProcessor;
 import com.resume.bot.json.entity.Client;
 import com.resume.hh_wrapper.ApiClientImpl;
 import com.resume.util.HHUriConstants;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -16,23 +18,23 @@ public class HeadHunterService {
     private ResumeService resumeService;
     private ApiClientImpl apiClient;
 
-    /*public Metro getMetro(int id) {
-        return get("metro/" + id, Metro.class);
-    }*/
-
     public void postCreateClient(String baseUri, Client client) {
-        apiClient.post(baseUri + HHUriConstants.POST_CREATE_RESUME_URI, client, Client.class);
+        apiClient.post(baseUri + HHUriConstants.POST_CREATE_RESUME_URI,
+                JsonProcessor.createJsonFromEntity(client), String.class);
     }
 
     public void putEditClient(String baseUri, String resumeId, Client client) {
-        apiClient.put(baseUri + HHUriConstants.PUT_EDIT_RESUME_URI.replace("{resume_id}", resumeId), client, Client.class);
+        apiClient.put(baseUri + HHUriConstants.PUT_EDIT_RESUME_URI.replace("{resume_id}", resumeId),
+                JsonProcessor.createJsonFromEntity(client), String.class);
     }
 
     public List<Client> getClientsResume(String baseUri) {
-        return apiClient.getList(baseUri + HHUriConstants.GET_RESUMES_URI, Client.class);
+        List<String> jsonStrings = apiClient.getList(baseUri + HHUriConstants.GET_RESUMES_URI, String.class);
+        return jsonStrings.stream().map(json -> JsonProcessor.createEntityFromJson(json, Client.class)).collect(Collectors.toList());
     }
 
     public Client getClientResume(String baseUri, String resumeId) {
-        return apiClient.get(baseUri + HHUriConstants.GET_RESUME_BY_ID_URI.replace("{resume_id}", resumeId), Client.class);
+        return JsonProcessor.createEntityFromJson(apiClient.get(baseUri
+                        + HHUriConstants.GET_RESUME_BY_ID_URI.replace("{resume_id}", resumeId), String.class), Client.class);
     }
 }
