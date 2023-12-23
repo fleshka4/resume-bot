097a5fcb660dc85c7e1902feda7219085ea086fb
Alexander Peskov <73636600+aosandy@users.noreply.github.com>
2023-12-23 16:00:46 +0300

    feat: add service methods (#27)

--- src/main/java/com/resume/bot/service/TemplateService.java
+++ src/main/java/com/resume/bot/service/TemplateService.java
@@ -1,7 +1,23 @@
 package com.resume.bot.service;
 
+import com.resume.bot.model.entity.Template;
 import com.resume.bot.repository.TemplateRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class TemplateService {
-    private TemplateRepository templateRepository;
+    private final TemplateRepository repository;
+
+    public Template saveTemplate(Template template) {
+        return repository.save(template);
+    }
+
+    public Template getTemplate(int templateId) {
+        return repository.findById(templateId).orElseThrow(
+                () -> new EntityNotFoundException("Template not found")
+        );
+    }
 }
--- src/main/java/com/resume/bot/service/TokenHolderService.java
+++ src/main/java/com/resume/bot/service/TokenHolderService.java
@@ -1,7 +1,20 @@
 package com.resume.bot.service;
 
+import com.resume.bot.model.entity.TokenHolder;
+import com.resume.bot.model.entity.User;
 import com.resume.bot.repository.TokenHolderRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class TokenHolderService {
-    private TokenHolderRepository repository;
+    private final TokenHolderRepository repository;
+
+    public TokenHolder getUserToken(User user) {
+        return repository.findByUser(user).orElseThrow(
+                () -> new EntityNotFoundException("Token not found")
+        );
+    }
 }
